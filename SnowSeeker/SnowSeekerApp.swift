4faa049a5f42e169aa3ad047a9b30ccc8d71113c
Scott Crowder <scrowder@gmail.com>
2024-03-21 16:19:35 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by SCOTT CROWDER on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
